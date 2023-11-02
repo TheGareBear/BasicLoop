da671331fda03c9bdd7b9750b51440c89e4c8469
GVassar <51205052+TheGareBear@users.noreply.github.com>
2023-11-02 14:10:20 -0600

    Basic loop

---
+++ Assets/Scenes/Spawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnPrefab;
+    public int spawnCount = 5;
+    public float spawnOffset = 1.5f;
+
+    void Start() 
+    {
+        if (spawnPrefab != null)
+        {
+            SpawnEnemies();
+        }
+        else
+        {
+            Debug.LogError("Cannot spawn enemies! Prefab is missing!");
+        }
+    }
+
+    void SpawnEnemies() {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            float xPos = i * spawnOffset;
+            Vector3 spawnPos = new Vector3(xPos, 0, 0);
+            Instantiate(spawnPrefab, spawnPos, Quaternion.identity);
+        }
+    }
+}
